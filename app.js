4e76674d78b82916f76210ec43bd7ca5e1d2bbab
Pedro Laguna <pdr.laguna@gmail.com>
2019-09-16 13:57:25 -0300

    Adding calcMod function
    Trying to calculate the stat modifier but still missing a reference to the sheet.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let sheet = {
 let standardArray = [15, 14, 13, 12, 10, 8];
 let atributes = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
 
-function remove(array, index) {
+function removeIndexFromArray(array, index) {
   return array.slice(0, index).concat(array.slice(index + 1))
 }
 
@@ -20,13 +20,15 @@ window.onload = function() {
     isAtributeValid = Number(prompt("Choose your " + atributes[i] + " value between " + standardArray))
     if(standardArray.includes(isAtributeValid)){
       sheet[atributes[i]] = isAtributeValid;
-      standardArray = remove(standardArray, standardArray.indexOf(isAtributeValid));
+      standardArray = removeIndexFromArray(standardArray, standardArray.indexOf(isAtributeValid));
 
-    }else i--;//deselegante as fuck
+    }else i--;
 
   }
   console.table(sheet)
-  console.log(Math.floor((sheet.dexterity-10)/2))
 }
-//next steps:
-//-receber atributos do usuario
+
+let calcMod = function(attribute){
+	let mod = Math.floor((sheet.attribute-10)/2);
+	console.log(mod);
+}
